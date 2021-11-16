31799e4e6e4bb5020c81cdec27bc77756e476c50
JDLanata <87950670+JDLanata@users.noreply.github.com>
2021-11-16 14:51:17 -0800

    changed Get workout and range methods to add aggrafate, got duration working

--- server.js
+++ server.js
@@ -28,24 +28,15 @@ app.get('/exercise', (req, res) => {
 });
 
 
-app.get("/api/workouts", (req, res) => {
-    db.Workout.find({},(err, data) =>{
-        if(err){
-            res.json(err)
-        }else{
-            res.json(data)
-        }
-    })
-});
 
 app.put("/api/workouts/:id", (req, res) => {
     db.Workout.findByIdAndUpdate({_id: req.params.id},{$push: {exercises: req.body}})
-       .then(dbWork => {
-           res.json(dbWork);
-       })
-       .catch(err => {
-           res.json(err);
-       });
+    .then(dbWork => {
+        res.json(dbWork);
+    })
+    .catch(err => {
+        res.json(err);
+    });
 })
 
 
@@ -59,18 +50,45 @@ app.post("/api/workouts", (req, res) => {
     });
 });
 
+app.get("/api/workouts", (req, res) => {
+    db.Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: { 
+                    $sum: "$exercises.duration" 
+                } 
+            }
+        }
+    ])
+    .then(data=>{
+        res.json(data)
+    }).catch(err=>{
+        console.log(err)
+        res.json(err)
+    })
+    
+});
 
 app.get("/api/workouts/range", (req, res) =>{
-    db.Workout.find({})
-    .populate("exercises")
-    .then(dbWork => {
-        res.json(dbWork);
+    db.Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: { 
+                    $sum: "$exercises.duration" 
+                } 
+            }
+        }
+    ])
+    .sort({_id:-1})
+    .limit(7)
+    .then(data=>{
+        res.json(data)
+    }).catch(err=>{
+        console.log(err)
+        res.json(err)
     })
-    .catch(err => {
-        res.json(err);
-    });
-
-
+    
+    
 })
 
 
