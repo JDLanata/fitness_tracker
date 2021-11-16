43fccf59c80535fbae7dfe0a03390f0ef67d66f1
JDLanata <87950670+JDLanata@users.noreply.github.com>
2021-11-16 15:18:13 -0800

    added mongo link

--- server.js
+++ server.js
@@ -16,7 +16,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: true
+    }
+  );
 
 
 
@@ -30,65 +38,65 @@ app.get('/exercise', (req, res) => {
 
 
 app.put("/api/workouts/:id", (req, res) => {
-    db.Workout.findByIdAndUpdate({_id: req.params.id},{$push: {exercises: req.body}})
-    .then(dbWork => {
-        res.json(dbWork);
-    })
-    .catch(err => {
-        res.json(err);
-    });
+    db.Workout.findByIdAndUpdate({ _id: req.params.id }, { $push: { exercises: req.body } })
+        .then(dbWork => {
+            res.json(dbWork);
+        })
+        .catch(err => {
+            res.json(err);
+        });
 })
 
 
 app.post("/api/workouts", (req, res) => {
     db.Workout.create({})
-    .then(dbWork => {
-        res.json(dbWork);
-    })
-    .catch(err => {
-        res.json(err);
-    });
+        .then(dbWork => {
+            res.json(dbWork);
+        })
+        .catch(err => {
+            res.json(err);
+        });
 });
 
 app.get("/api/workouts", (req, res) => {
     db.Workout.aggregate([
         {
             $addFields: {
-                totalDuration: { 
-                    $sum: "$exercises.duration" 
-                } 
+                totalDuration: {
+                    $sum: "$exercises.duration"
+                }
             }
         }
     ])
-    .then(data=>{
-        res.json(data)
-    }).catch(err=>{
-        console.log(err)
-        res.json(err)
-    })
-    
+        .then(data => {
+            res.json(data)
+        }).catch(err => {
+            console.log(err)
+            res.json(err)
+        })
+
 });
 
-app.get("/api/workouts/range", (req, res) =>{
+app.get("/api/workouts/range", (req, res) => {
     db.Workout.aggregate([
         {
             $addFields: {
-                totalDuration: { 
-                    $sum: "$exercises.duration" 
-                } 
+                totalDuration: {
+                    $sum: "$exercises.duration"
+                }
             }
         }
     ])
-    .sort({_id:-1})
-    .limit(7)
-    .then(data=>{
-        res.json(data)
-    }).catch(err=>{
-        console.log(err)
-        res.json(err)
-    })
-    
-    
+        .sort({ _id: -1 })
+        .limit(7)
+        .then(data => {
+            res.json(data)
+        }).catch(err => {
+            console.log(err)
+            res.json(err)
+        })
+
+
 })
 
 
